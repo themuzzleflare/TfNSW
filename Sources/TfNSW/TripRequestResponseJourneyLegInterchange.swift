ad0b03f5a4e2965ae497f35df91215a14bbb8c17
Paul Tavitian <paul.tavitian@icloud.com>
2021-08-31 13:49:23 +1000

    25.0.0

--- Sources/TfNSW/TripRequestResponseJourneyLegInterchange.swift
+++ Sources/TfNSW/TripRequestResponseJourneyLegInterchange.swift
@@ -5,7 +5,7 @@ public struct TripRequestResponseJourneyLegInterchange: Decodable {
   public var desc: String?
 
   /// This indicates the mode of travel for the interchange. Both `99` and `100` indicate walking. = ['99', '100'].
-  public var type: LocationType?
+  public var type: ProductClass?
 
   /// This is a list of coordinates that makes up the path of the interchange.
   public var coords: [[Double]]?
