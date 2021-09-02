924cbbb089f1b5c6b7e5ac70e4bc983008afbdf3
Paul Tavitian <paul.tavitian@icloud.com>
2021-09-02 13:53:22 +1000

    35.0.0

--- Sources/TfNSW/PropertyWrappers/StopName.swift
+++ Sources/TfNSW/PropertyWrappers/StopName.swift
@@ -3,9 +3,17 @@ import Foundation
 @propertyWrapper public struct StopName: Decodable {
   public var wrappedValue: String?
 
+  public init(wrappedValue: String?) {
+    self.wrappedValue = wrappedValue?
+      .replacingOccurrences(of: "Macquarie University", with: "Macquarie Uni")
+      .replacingOccurrences(of: "International Airport", with: "Intnl Airport")
+  }
+
   public init(from decoder: Decoder) throws {
     let value = try decoder.singleValueContainer()
     let stringValue = try value.decode(String.self)
-    wrappedValue = stringValue.replacingOccurrences(of: "University", with: "Uni")
+    wrappedValue = stringValue
+      .replacingOccurrences(of: "Macquarie University", with: "Macquarie Uni")
+      .replacingOccurrences(of: "International Airport", with: "Intnl Airport")
   }
 }
