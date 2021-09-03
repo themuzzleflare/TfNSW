feb2897f49dc2ac41793b8c3c4fdc89fdae920ca
Paul Tavitian <paul.tavitian@icloud.com>
2021-09-03 11:56:21 +1000

    41.0.0

--- Sources/TfNSW/ParentLocation.swift
+++ Sources/TfNSW/ParentLocation.swift
@@ -29,7 +29,7 @@ public struct ParentLocation: Decodable {
     isGlobalId = try values.decodeIfPresent(Bool.self, forKey: .isGlobalId)
     id = try values.decodeIfPresent(String.self, forKey: .id)
     name = try values.decodeIfPresent(String.self, forKey: .name)
-    disassembledName = try values.decodeIfPresent(String.self, forKey: .disassembledName)
+    disassembledName = try values.decode(String.self, forKey: .disassembledName)
     type = try values.decodeIfPresent(LocationType.self, forKey: .type)
     parent = try values.decodeIfPresent(GrandparentLocation.self, forKey: .parent)
     properties = try values.decodeIfPresent(ParentProperties.self, forKey: .properties)
