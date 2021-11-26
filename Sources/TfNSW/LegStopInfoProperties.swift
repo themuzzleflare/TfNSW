217e22bae63fd373d516745a90db68037dd3bc5e
Paul Tavitian <paul.tavitian@icloud.com>
2021-11-26 11:01:06 +1100

    47.0.0.

--- Sources/TfNSW/LegStopInfoProperties.swift
+++ Sources/TfNSW/LegStopInfoProperties.swift
@@ -1,9 +1,15 @@
 import Foundation
 
 public struct LegStopInfoProperties: Decodable {
+  public var publisher: String?
+  
+  public var infoType: String?
+  
   public var appliesTo: String?
 
   public var stopIDglobalID: String?
+  
+  public var smsText: String?
 
   public var speechText: String?
 }
--- Sources/TfNSW/TripRequestResponseJourneyLegStopInfo.swift
+++ Sources/TfNSW/TripRequestResponseJourneyLegStopInfo.swift
@@ -18,12 +18,12 @@ public struct TripRequestResponseJourneyLegStopInfo: Decodable {
 
   /// This field contains a URL that contains additional information about the alert. This value is equivalent to the `infoLinkURL` value in `add_info`.
   public var url: String?
+  
+  /// This is the descriptive alert content. It may contain HTML tags and/or HTML entities.
+  public var content: String?
 
   /// This is short summary that can be used as a heading for the alert content. It may contain HTML tags and/or HTML entities.
   public var subtitle: String?
-
-  /// This is the descriptive alert content. It may contain HTML tags and/or HTML entities.
-  public var content: String?
 
   public var properties: LegStopInfoProperties?
 }
